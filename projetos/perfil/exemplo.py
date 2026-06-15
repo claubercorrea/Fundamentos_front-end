eff693107d3a4f5ed21c024530a8b6ea30f9a7d2
CLAUBER CORREA <162238096+claubercorrea@users.noreply.github.com>
2026-06-15 02:46:51 -0300

    Delete projetos directory

--- projetos/perfil/exemplo.py
+++
@@ -1,8 +0,0 @@
-saldo = float(input('saldo'))
-print(f'seu saldo {saldo}')
-
-print('saldo de emergencia ')
-emergencia= float('')
-
-emergencial  = saldo * 0.10
-saldo_restante = saldo - emergencial 
